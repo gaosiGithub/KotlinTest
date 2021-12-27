d094e2c32a16a3f8f9f4fa09ca598a4113024c71
gaosi <si.gao@tcl.com>
2021-12-27 14:20:21 +0800

    初始化app

---
+++ app/src/main/java/com/gaosi/kotlinplay/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gaosi.kotlinplay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
